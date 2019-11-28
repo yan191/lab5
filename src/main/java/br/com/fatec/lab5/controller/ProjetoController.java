ec3c183e129f860e9ce060f8b71f99e69114ff6e
Yan Rodrigues <yan.azevedo@gsw.com.br>
2019-11-28 15:33:49 -0300

    Ajustando rotas

--- src/main/java/br/com/fatec/lab5/controller/ProjetoController.java
+++ src/main/java/br/com/fatec/lab5/controller/ProjetoController.java
@@ -5,13 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fatec.lab5.model.Apontamento;
 import br.com.fatec.lab5.model.Projeto;
+import br.com.fatec.lab5.service.ApontamentoService;
 import br.com.fatec.lab5.service.ProjetoService;
 
 @RestController
@@ -20,7 +23,11 @@ public class ProjetoController {
 	@Autowired
 	ProjetoService projetoService;
 	
+	@Autowired
+	ApontamentoService apontamentoService;
+	
 	@PostMapping("/projeto")
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Projeto> createNewProjeto(@RequestBody Projeto project){
 		Projeto projeto;
 		try {			
@@ -32,15 +39,24 @@ public class ProjetoController {
 	}
 	
 	@GetMapping("/projeto")
+	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	public ResponseEntity<List<Projeto>> findAllProjeto(){
 		List<Projeto> projetos = projetoService.findAll();
 		return ResponseEntity.ok().body(projetos);
 	}
 	
 	@GetMapping("/projeto/{projetoId}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Optional<Projeto>> findProjetoById(@PathVariable Integer projetoId){
 		Optional<Projeto> projeto = projetoService.findById(projetoId);
 		return ResponseEntity.ok().body(projeto);
+	}
+	
+	@GetMapping("/projeto/{projetoId}/apontamento")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<List<Apontamento>> findApontamentosByProjetoId(@PathVariable Integer projetoId){
+		List<Apontamento> apontamentos = apontamentoService.findApontamentosByProjetoId(projetoId);
+		return ResponseEntity.ok().body(apontamentos);
 	}
 	
 }
